27ed0d61e4429e89962a4e9e6caf4a3ad191c583
Leon V <33411715+leonv024@users.noreply.github.com>
2019-03-01 22:24:29 +0100

    It's now a server that monitors the websites for (multiple) jobs

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import os, sys, time,argparse
-from tqdm import tqdm
 from prettytable import PrettyTable
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -8,9 +7,10 @@ from bs4 import BeautifulSoup
 def parse_args():
     # Create the arguments
     parser = argparse.ArgumentParser(prog='Job Hunter')
-    parser.add_argument("-jt", "--jobtitle", help="Job Title. Example: -jt System Administrator")
+    parser.add_argument("-jt", "--jobtitle", help="Job Title. Example: -jt System Administrator/Ethical hacker/Linux System Administrator")
     parser.add_argument("-l", "--location", help="Location. Example: -l Berlin")
-    parser.add_argument("-z", "--zipcode", help="Zipcode. Example: -l 3078TR")
+    parser.add_argument("-z", "--zipcode", help="Zipcode. Example: -z 3078TR")
+    parser.add_argument("-i", "--interval", help="Interval to search for jobs in seconds (default: 10). Example: -i 10")
     return parser.parse_args()
 
 args = parse_args()
@@ -29,7 +29,7 @@ def search_indeed(jobtitle, location):
     url1 = 'https://www.indeed.nl/jobs?q=%s&l=%s' % (jobtitle.replace(' ', '+'), location)
     soup = BeautifulSoup(urlopen(url1), 'html.parser')
 
-    print('[i] Searching for jobs on Indeed.nl')
+    #print('[i] Searching for jobs as [%s] on Indeed.nl' % jobtitle)
 
     find_company_names = soup.find_all('span', attrs={'class': 'company'})
     find_job_titles = soup.find_all('a', attrs={'data-tn-element': 'jobTitle'})
@@ -49,7 +49,7 @@ def search_indeed(jobtitle, location):
     url1 = 'https://www.indeed.nl/jobs?q=%s&l=%s&start=10' % (jobtitle.replace(' ', '+'), location)
     soup = BeautifulSoup(urlopen(url1), 'html.parser')
 
-    print('[i] Searching for jobs on Indeed.nl on page 2')
+    #print('[i] Searching for jobs as [%s] on Indeed.nl on page 2' % jobtitle)
 
     find_company_names = soup.find_all('span', attrs={'class': 'company'})
     find_job_titles = soup.find_all('a', attrs={'data-tn-element': 'jobTitle'})
@@ -70,7 +70,7 @@ def search_stagemarkt(jobtitle, zip):
     url2 = 'https://stagemarkt.nl/Zoeken/Home/Resultaten?t=%s&s=5&z=&l=Nederland&b=False&c=&lw=&n=&pg=1&srt=&e=false&ToonOpKaart=False&ViewType=Lijst&SeedValue=0&LeerbedrijfId=0&p=%s' % (jobtitle.replace(' ', '+'), zip)
     soup = BeautifulSoup(urlopen(url2), 'html.parser')
 
-    print('[i] Searching for jobs on Stagemarkt.nl')
+    #print('[i] Searching for jobs as [%s] on Stagemarkt.nl' % jobtitle)
 
     find_company_names = soup.find_all('div', attrs={'class': 'list__item__title'})
 
@@ -83,7 +83,7 @@ def search_stagemarkt(jobtitle, zip):
     url2 = 'https://stagemarkt.nl/Zoeken/Home/Resultaten?t=%s&s=5&z=&l=Nederland&b=False&c=&lw=&n=&pg=2&srt=&e=false&ToonOpKaart=False&ViewType=Lijst&SeedValue=0&LeerbedrijfId=0&p=%s' % (jobtitle.replace(' ', '+'), zip)
     soup = BeautifulSoup(urlopen(url2), 'html.parser')
 
-    print('[i] Searching for jobs on Stagemarkt.nl on page 2')
+    #print('[i] Searching for jobs as [%s] on Stagemarkt.nl on page 2' % jobtitle)
 
     find_company_names = soup.find_all('div', attrs={'class': 'list__item__title'})
 
@@ -92,24 +92,59 @@ def search_stagemarkt(jobtitle, zip):
         titles.append(jobtitle)
         websites.append('https://www.stagemarkt.nl')
 
-table = PrettyTable(['Num.', 'Job Title', 'Company', 'Website']) # Header
-table.align = "l" # Text Align left
+def result():
+    c = len(companies)
+    filepath = './log.csv'
+
+    # Check if logfile exists, if not, create it
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w+') as f:
+            f.write('Num.,Job Title,Company,Website\n')
+            f.close()
+        print('[+] Created logfile at %s' % filepath)
+
+    for i in range(len(companies)):
+        result = str(c) + ')', titles[i], companies[i], websites[i]
+        check = open(filepath).read()
+
+        # If it's not a duplicate, print and save result
+        if not companies[i] in check:
+            c +=1
+            print('%s %s %s %s' % (result[0].ljust(10), result[1].ljust(60), result[2].ljust(60), result[3].ljust(40)))
+            with open(filepath, 'a+') as f:
+                f.write('%s,%s,%s,%s\n' % (result[0], result[1], result[2], result[3]))
+                f.close()
+
+if not args.interval:
+    interval = 5
+else:
+    interval = args.interval
 
 # Run
 if args.jobtitle == None or args.location == None or args.zipcode == None:
     print('[ERROR] Please give a jobtitle, location, zipcode...')
 else:
-    search_indeed(args.jobtitle, args.location)
-    search_stagemarkt(args.jobtitle, args.zipcode)
-    c = 0
+    try:
+        job = args.jobtitle.split('/')
+        for i in job:
+            print('[i] Monitoring for jobs as %s' % i)
 
-    for i in range(len(companies)):
-        c +=1
-        result = int(c), titles[i], companies[i], websites[i]
-        table.add_row([result[0], result[1], result[2], result[3]])
-    print(table)
-    print('Found %i jobs\n' % len(companies))
-
-    print('Source:')
-    print('Indeed URL: %s' % url1)
-    print('Stagemarkt URL: %s' % url2)
+        try:
+            while True:
+                for i in job:
+                    search_indeed(i, args.location)
+                    search_stagemarkt(i, args.zipcode)
+                    result()
+                time.sleep(interval)
+        except KeyboardInterrupt:
+            print('[i] Stopped...\n')
+    except:
+        print('[i] Monitoring for jobs as %s' % args.jobtitle)
+        try:
+            while True:
+                search_indeed(args.jobtitle, args.location)
+                search_stagemarkt(args.jobtitle, args.zipcode)
+                result()
+                time.sleep(interval)
+        except KeyboardInterrupt:
+            print('[i] Stopped...\n')
